ae475068fe8a26c085162415d03b3249e91e346f
Ocheretko1992 <117608037+Ocheretko1992@users.noreply.github.com>
2024-05-07 05:10:39 +0300

    )(

---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,14 @@
+export default function OptoinsBtn() {
+
+  const clic = function () {
+   
+  }
+  return (
+    <div>
+      <button onMouseOver={clic}>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <button>Reset</button>
+    </div>
+  );
+}
